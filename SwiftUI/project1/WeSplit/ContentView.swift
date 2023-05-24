64a710363fa38fc2e4835c8cb94d1b26a042cfbd
Warkentin Erwin <warkentinerwin@icloud.com>
2023-05-24 15:06:29 +0200

    added some Header, a subtotal and percentage 0 to 100

--- SwiftUI/project1/WeSplit/ContentView.swift
+++ SwiftUI/project1/WeSplit/ContentView.swift
@@ -1,77 +1,75 @@
-//
-//  ContentView.swift
-//  WeSplit
-//
-//  Created by Paul Hudson on 16/10/2021.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @FocusState private var amountIsFocused: Bool
+    
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    @FocusState private var amountIsFocused: Bool
-    
-    let tipPercentages = [10, 15, 20, 25, 0]
     
     var totalPerPerson: Double {
         let peopleCount = Double(numberOfPeople + 2)
-        let tipSelection = Double(tipPercentage)
         
-        let tipValue = checkAmount / 100 * tipSelection
-        let grandTotal = checkAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
-        
+        let amountPerPerson = subTotal / peopleCount
+
         return amountPerPerson
     }
     
+    var subTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let subTotal = checkAmount + tipValue
+        
+        return subTotal
+    }
+
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    TextField("Betrag", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
                     
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2..<100) {
-                            Text("\($0) people")
+                    
+                    Picker("Anzahl der Leute", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) Leute")
                         }
                     }
                 }
-                
                 Section {
-                    Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages, id: \.self) {
+                    Picker("Trinkgeld in Prozent", selection: $tipPercentage) {
+                        ForEach(0..<101) {
                             Text($0, format: .percent)
                         }
                     }
-                    .pickerStyle(.segmented)
+                    .pickerStyle(.menu)
                 } header: {
-                    Text("How much tip do you want to leave?")
+                    Text("Wie viel Trinkgeld wollt ihr da lassen?")
                 }
-                
                 Section {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                    Text(subTotal, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
+                }header: {
+                    Text("Zwischensumme (Rechnung + Trinkgeld)")
+                }
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
+                }header: {
+                    Text("Betrag pro Person")
                 }
             }
             .navigationTitle("WeSplit")
             .toolbar {
                 ToolbarItemGroup(placement: .keyboard) {
                     Spacer()
-                    
+
                     Button("Done") {
                         amountIsFocused = false
                     }
                 }
             }
         }
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
